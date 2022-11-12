6a041cf3130731aaa77baf73b6ddc900db31a686
Seva <dancingfallingleaves@gmail.com>
2022-11-12 03:30:17 +0300

    added new stuff

--- pygame_lesson/config.py
+++ pygame_lesson/config.py
@@ -1,28 +1,23 @@
-# ------------ Colors ------------
+# colors
+BLUE = (0, 152, 236)
+RED = (236, 0, 0)
+YELLOW = (255, 224, 22)
+ORANGE = (255, 140, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-BLUE = (20, 217, 217)
-RED = (235, 64, 52)
-YELLOW = (255, 252, 71)
-ORANGE = (255, 127, 15)
-BLACK = (31, 31, 31)
-WHITE = (250, 250, 250)
-
-
-# ------------ Board sizes ------------
+# Rows/columns
 ROWS = 6
 COLUMNS = 7
 
-# ------------ Disc sizes ------------
 DISC_SIZE = 80
-DISC_RADUIS = int(DISC_SIZE/2 - 10)
+DISC_RADIUS = int(DISC_SIZE / 2 - 10)
 
-# ------------ Players ------------
-PLAYER = 0
-COMP = 1
+REAL_PLAYER = 0
+AI_PLAYER = 1
 
 EMPTY = 0
-PLAYER_CHIP = 1
-COMP_CHIP = 2
-
+REAL_PLAYER_PIECE = 1
+AI_PLAYER_PIECE = 2
 
 MAIN_WINDOW = 4
---
+++ template/board.py
@@ -0,0 +1,15 @@
+import numpy as np
+from config import *
+
+
+def new_grid():
+
+
+def is_valid_position(grid, col):
+
+
+def get_valid_position(grid):
+
+
+
+def get_next_open_row(grid, c):
